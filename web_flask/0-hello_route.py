30f856577dc84d7656c358b9957c1e0e2df4e74c
Tshimangadzo <mudzanani27@gmail.com>
2023-04-26 15:41:03 +0200

    added the code comments

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,15 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/',strict_slashes=False)
 def hello_HBNB():
+    """returns Hello HBNB!"""
+    
     return 'Hello HBNB!'
 
 
